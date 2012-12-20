8c51e17d42ad63e614cd00305e7c52ff6ab3bb21
Scott Lampert <sl724q@att.com>
2012-12-20 14:03:21 -0800

    Merged in Josh's changes.

--- hiverify.sh
+++ hiverify.sh
@@ -5,21 +5,23 @@
 
 function print_usage {
   echo
-  echo "Usage: ${0} [site name] <netcat remote IP]>"
+echo "Usage: ${0} [site name] <netcat remote IP]>"
   echo
-  echo "Leaving netcat IP blank will give human readable output."
+echo "Leaving netcat IP blank will give human readable output."
 }
 
 if [ -z ${1} ]
 then
-  echo "Need site name (i.e. SAN, DFW, EWR, etc)."
+echo "Need site name (i.e. SAN, DFW, EWR, etc)."
   print_usage
   exit 1
 fi
 
 SITE=${1}
+HOST=`hostname -f`
 ADMIN_NAME=""
-if dpkg --get-selections | grep -q clamav && [ $? = 0 ]; then VIRUS_TYPE="clamav" else VIRUS_TYPE="none installed"; fi
+DATE=`date +%Y/%m/%d`
+if dpkg --get-selections | grep -q clamav && [ $? = 0 ]; then VIRUS_TYPE="clamav"; else VIRUS_TYPE="none installed"; fi
 VIRUS_SIG_FILE="Linux"
 VIRUS_UPDATE="daily"
 if grep -q "AT&T" /etc/motd && [ $? = 0 ]; then CONF_LOGIN_BANNER="Yes"; else CONF_LOGIN_BANNER="No"; fi
@@ -28,31 +30,31 @@ CONF_TIME_SYNC=`grep ^server /etc/ntp.conf | awk '{print $2}' | paste -s --delim
 REMOTE_ACCESS="ssh"
 UNSECURED_PROTOCOL="All accounted for."
 UNNECESSARY_PORTS="Firewall"
-PATCH_STATUS=`tail -1 /var/log/dpkg.log | awk '{print $1}'`
-PW_MIN_LEN="SSH Key"
-PW_COMPLEXITY="SSH Key"
-PW_ROTATION="None"
+PATCH_STATUS=`/usr/lib/nagios/plugins/check_apt | egrep -o '\(.*\)'`
+PW_MIN_LEN=8
+PW_COMPLEXITY="meets policy"
+PW_ROTATION="SSH Key"
 PW_NO_DEFAULT="change immediately"
-PW_NO_DISPLAY="Hidden"
+PW_NO_DISPLAY="hidden"
 PW_REUSE="SSH Key"
 PW_LOCKOUT="On Demand"
-PW_LOCKOUT_DURATION="As Needed"
+PW_LOCKOUT_DURATION="As needed"
 ACCESS_SESSION_TIMEOUT="8 hours"
 ACCESS_NO_DEFAULT_USER="no default accounts"
 ACCESS_NO_DEFAULT_STRINGS="n/a"
 ACCESS_DISABLE_GUEST="no guest accounts"
 ACCESS_NO_DEFAULT_ACCOUNTS="no default accounts"
+ACCESS_NINETY_DAY_INACTIVE="No"
+ACCESS_UNIQUE_IDS="Yes"
 
-LISTEN_PORTS=`netstat -lnat | grep LISTEN | awk '{print $4}' |  sort | uniq | paste -s --delimiters=","`
-PENDING_UPDATES=`/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 2`
-
+LISTEN_PORTS=`netstat -lnat | grep LISTEN | grep 75.* | awk -F'[: ]+' '{print $5}' | sort | uniq | paste -s --delimiters=","`
 
 if [ -z ${2} ]
 then
 # human readable
 
 echo "Site: ${SITE}"
-echo "Hostname: ${HOSTNAME}"
+echo "Hostname: ${HOST}"
 echo "Admin Name: ${ADMIN_NAME}"
 echo "Virus Type: ${VIRUS_TYPE}"
 echo "Virus Sig: ${VIRUS_SIG_FILE}"
@@ -78,9 +80,9 @@ echo "Default Strings: ${ACCESS_NO_DEFAULT_STRINGS}"
 echo "Disable Guest: ${ACCESS_DISABLE_GUEST}"
 echo "Default Accounts: ${ACCESS_NO_DEFAULT_ACCOUNTS}"
 echo "Listening Ports: ${LISTEN_PORTS}"
-echo "Number of Pending Updates: ${PENDING_UPDATES}"
-
+echo "90+ Days Inactive: ${ACCESS_NINETY_DAY_INACTIVE}"
+echo "Unique IDs: ${ACCESS_UNIQUE_IDS}"
 else
-echo # netcat comma-delimited output
+echo "Listen Ports: ${LISTEN_PORTS}"
+echo "'${HOST}','${DATE}','','${VIRUS_TYPE}','${VIRUS_SIG_FILE}','${VIRUS_UPDATE}','${CONF_LOGIN_BANNER}','${CONF_LOG_FORWARDING}','${CONF_TIME_SYNC}','${REMOTE_ACCESS}','${UNSECURED_PROTOCOL}','${UNNECESSARY_PORTS}','${PATCH_STATUS}','${PW_MIN_LEN}','${PW_COMPLEXITY}','${PW_ROTATION}','${PW_NO_DEFAULT}','${PW_NO_DISPLAY}','${PW_REUSE}','${PW_LOCKOUT}','${PW_LOCKOUT_DURATION}','${ACCESS_SESSION_TIMEOUT}','${ACCESS_NO_DEFAULT_USER}','${ACCESS_NO_DEFAULT_STRINGS}','${ACCESS_DISABLE_GUEST}','${ACCESS_NO_DEFAULT_ACCOUNTS}','${ACCESS_NINETY_DAY_INACTIVE}','${ACCESS_UNIQUE_IDS}'"
 fi
-
